2190e360e8abfcd57fecb36c29dc9ea3c6452f08
Janendra de silva <your-github-email@example.com>
2025-09-04 20:21:27 +0530

    Initial commit

---
+++ src/main/java/org/example/srilanka_tour_and_travel/SriLankaTourAndTravelApplication.java
@@ -0,0 +1,13 @@
+package org.example.srilanka_tour_and_travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SriLankaTourAndTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SriLankaTourAndTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/srilanka_tour_and_travel/SriLankaTourAndTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.srilanka_tour_and_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriLankaTourAndTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
